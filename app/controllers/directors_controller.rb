5a8788e9dd691bc83a71644c4d5131f15ad2cffa
Nate Wong <nathaniel.h.wong@gmail.com>
2016-11-01 20:35:10 -0500

    Finished most of directors

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -8,15 +8,40 @@ class DirectorsController < ApplicationController
     @director = Director.find(params[:id])
   end
 
-  def edit_director
-      redirect_to("/directors")
+  def edit_director_form
+    @director = Director.find(params[:id])
   end
 
+  def edit_director
+    @director = Director.find(params[:id])
+    @director.name = params[:name]
+    @director.bio = params[:bio]
+    @director.dob = params[:dob]
+    @director.image_url = params[:url]
+    @director.save
+
+    redirect_to("/directors")
+  end
+
+
   def delete_director
-      redirect_to("/directors")
+    @director = Director.find(params[:id])
+    @director.delete
+
+    redirect_to("/directors")
   end
 
   def new_director
+  end
+
+  def create_director
+    @director = Director.new
+    @director.name = params[:name]
+    @director.bio = params[:bio]
+    @director.dob = params[:dob]
+    @director.image_url = params[:url]
+    @director.save
+
     redirect_to("/directors")
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,10 @@ Rails.application.routes.draw do
 
   get("/directors", { :controller => "directors", :action => "index" })
   get("/directors/:id", { :controller => "directors", :action => "show" })
-  get("/edit_director/:id", { :controller => "directors", :action => "edit_director" })
+  get("/edit_director_form/:id", { :controller => "directors", :action => "edit_director_form" })
   get("/delete_director/:id", { :controller => "directors", :action => "delete_director" })
-  get("/new_director/", { :controller => "directors", :action => "new_director" })
+  get("/new_director_form", { :controller => "directors", :action => "new_director_form" })
+  get("/create_director", { :controller => "directors", :action => "create_director" })
+  get("/edit_director/:id", { :controller => "directors", :action => "edit_director" })
 
 end
